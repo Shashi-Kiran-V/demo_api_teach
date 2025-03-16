0629ab322ac5bb3a1db8053e9b0ea66d54881155
Shashi-Kiran-V <shashikiranshetty02@gmail.com>
2025-03-16 21:19:33 +0530

    api calling via axios code updated in js and html file

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@ let url='https://catfact.ninja/fact';
 let first=document.getElementById('first');
 let second=document.getElementById('second')
 let button=document.getElementById('btn')
-console.log(first);
+//console.log(first);
 
 
-//2 methods of api calling
+//3 methods of api calling
 
 //1st method using fetch()
 
@@ -20,22 +20,41 @@ console.log(first);
 
 //2nd method using aync await
 
-async function getFacts() {
+// async function getFacts() {
+//     try{
+//     let res1= await fetch(url)
+//     let data1 = await res1.json()
+//     let res2= await fetch(url)
+//     let data2=await res2.json()
+//     first.innerText=data1.fact
+//     second.innerText=data2.fact
+//     console.log(data1.fact);
+//     console.log(data2.fact);
+//     }
+//     catch (e){
+//         console.log(e);
+        
+//     }
+     
+// }
+// button.onclick=getFacts
+
+//3rd method using axios
+//we use axios because we need not to parse the response. it internally uses fetch()
+
+async function getFacts(){
     try{
-    let res1= await fetch(url)
-    let data1 = await res1.json()
-    let res2= await fetch(url)
-    let data2=await res2.json()
-    first.innerText=data1.fact
-    second.innerText=data2.fact
-    console.log(data1.fact);
-    console.log(data2.fact);
-    }
-    catch (e){
-        console.log(e);
+        let res1= await axios.get(url);
+        //console.log(res1.data.fact);
+        let res2= await axios.get(url);
+        //console.log(res2.data.fact);
+        first.innerText=res1.data.fact;
+        second.innerText=res2.data.fact;
         
     }
-     
+    catch(e){
+        console.log((e));
+        
+    }
 }
 button.onclick=getFacts
-//getFacts()
